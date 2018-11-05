b94f9e5731c4470b89e7a059e3939f18f933d3ba
Michael Weimann <mail@michael-weimann.eu>
2018-11-05 20:17:00 +0100

    Adapts the code to the new master state
    Signed-off-by: Michael Weimann <mail@michael-weimann.eu>

--- apps/files_sharing/tests/Controller/ShareControllerTest.php
+++ apps/files_sharing/tests/Controller/ShareControllerTest.php
@@ -505,7 +505,8 @@ class ShareControllerTest extends \Test\TestCase {
 			'shareUrl' => '',
 			'previewImage' => '',
 			'previewURL' => '',
-			'note' => ''
+			'note' => '',
+			'hideDownload' => false
 		);
 
 		$csp = new \OCP\AppFramework\Http\ContentSecurityPolicy();
